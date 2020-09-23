6d1321b437ad9ee57bfe13a27a3f845009c02cc7
杨葱 <cong.yang@baozun.com>
2020-09-23 15:36:32 +0800

    修繕monad 去掉了不實在interface 而直接使用monad

--- src/main/java/com/example/pattern_in_action/monad/GenericValidator.java
+++ src/main/java/com/example/pattern_in_action/monad/GenericValidator.java
@@ -1,16 +1,45 @@
 package com.example.pattern_in_action.monad;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public interface GenericValidator<T> {
+
+@Getter
+public class GenericValidator<T> {
+
+    private final T obj;
+
+    private final List<String> exceptions = new ArrayList<>();
+
+    private GenericValidator(T obj) {
+        this.obj = obj;
+    }
+
+    public static <T> GenericValidator<T> unit(T t) {
+        return new GenericValidator<>(Objects.requireNonNull(t));
+    }
 
 
-    GenericValidator<T> validate(Predicate<T> p, String message);
+    public GenericValidator<T> validate(Predicate<T> p, String message) {
+        if (!p.test(obj)) {
+            exceptions.add(message);
+        }
+        return this;
+    }
 
-    <U> GenericValidator<T> mapValidate(Function<T, U> f, Predicate<U> p, String message);
 
-    <U> GenericValidator<U> flatMap(Function<T, U> f,Function<U,GenericValidator<U>> validator);
+    public <U> GenericValidator<T> mapValidate(Function<T, U> f, Predicate<U> p, String message) {
+        return validate(f.andThen(p::test)::apply, message);
+    }
 
+
+    public <U> GenericValidator<U> flatMap(Function<T, U> f, Function<U, GenericValidator<U>> validator) {
+        return f.andThen(validator::apply).apply(obj);
+    }
 
 }
--- src/main/java/com/example/pattern_in_action/monad/Person.java
+++ src/main/java/com/example/pattern_in_action/monad/Person.java
@@ -1,18 +1,25 @@
 package com.example.pattern_in_action.monad;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@ApiModel("person")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person {
 
+    @ApiModelProperty(value = "name", example = "onion")
     private String name;
+    @ApiModelProperty(value = "age", example = "27")
     private int age;
+    @ApiModelProperty(value = "sex")
     private Sex sex;
+    @ApiModelProperty(value = "email", example = "2703012@yuyixi.com")
     private String email;
 
 
