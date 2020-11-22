232c800b03b7ee951145f32b87c6ebeac9dcb2fd
杨葱 <qingluanxi@gmail.com>
2020-11-22 22:13:46 +0800

    change sdk

--- src/main/java/com/example/pattern_in_action/controller/AmbassadorController.java
+++ src/main/java/com/example/pattern_in_action/controller/AmbassadorController.java
@@ -1,36 +1,36 @@
-//package com.example.pattern_in_action.controller;
-//
-//import com.example.pattern_in_action.ambassador.Client;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//
-//@RestController
-//@RequestMapping("/ambassador/")
-//public class AmbassadorController {
-//
-//
-//    @PostMapping("callService")
-//    public Object callService(@RequestParam("num") Integer num) {
-//        ExecutorService threadPool = Executors.newFixedThreadPool(2000);
-//        while (num >= 0) {
-//            final var numCopy = num;
-//            threadPool.execute(() -> {
-//                Client client = new Client();
-//                client.callLogicService(numCopy);
-//            });
-//            num--;
-//        }
-//        while (true) {
-//            threadPool.shutdown();
-//            if (threadPool.isTerminated()) {
-//                return "ok";
-//
-//            }
-//        }
-//    }
-//}
+package com.example.pattern_in_action.controller;
+
+import com.example.pattern_in_action.ambassador.Client;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
+@RequestMapping("/ambassador/")
+public class AmbassadorController {
+
+
+    @PostMapping("callService")
+    public Object callService(@RequestParam("num") Integer num) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2000);
+        while (num >= 0) {
+            final var numCopy = num;
+            threadPool.execute(() -> {
+                Client client = new Client();
+                client.callLogicService(numCopy);
+            });
+            num--;
+        }
+        while (true) {
+            threadPool.shutdown();
+            if (threadPool.isTerminated()) {
+                return "ok";
+
+            }
+        }
+    }
+}
